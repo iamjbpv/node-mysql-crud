ccfe06cfb68bad053d672ac958a75e9d666465ec
Jb <iamjbpv@outlook.com>
2020-04-02 00:18:43 +0800

    change connection settings and added create table

--- connection.js
+++ connection.js
@@ -12,6 +12,27 @@ const getConnection = function(done){
    connectionPool.getConnection(done);
 };
 
+getConnection(function(error, connection){
+    if(!error) {
+        var sql = `CREATE TABLE IF NOT EXISTS users (
+            id int(11) NOT NULL AUTO_INCREMENT,
+            email varchar(50) DEFAULT NULL,
+            first_name varchar(50) DEFAULT NULL,
+            last_name varchar(50) DEFAULT NULL,
+            password varchar(255) DEFAULT NULL,
+            is_deleted tinyint(1) NOT NULL DEFAULT '0',
+            PRIMARY KEY (id)
+          )`;
+        connection.query(sql, function (err, result) {
+            if (err){
+                throw err.message;
+            }
+        });
+    } else {
+        throw error.errno;
+    }
+});
+
 module.exports = {getConnection: getConnection};
 
 
